bd7f8d6c4427d19cda6a53b6d7ebe317fbaff08f
jsramos <ramos.cardona@gmail.com>
2015-02-18 22:48:19 -0600

    Uploaded final version of programming assignment #2 for the Coursera R Programming course.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R script to provide 2 functions: 1 to store a matrix in a variable as cache,
+# and another to compute its inverse and cache it as well       
 
-## Write a short comment describing this function
+## Caches matrix x
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        # Internal variable for storing the inverted matrix        
+        i <- NULL 
+        # Overrides matrix x from whatever was in the argument to a new value.
+        # Also clears cache variable for inverted matrix i.
+        set <- function(y) { 
+                x <<- y # x is the old matrix, y is the new matrix
+                i <<- NULL # i is cleared
+        }
+        # Returns the value of matrix x
+        get <- function() {x}
+        # Sets the value for cache variable i to the argument inverse
+        setInverse <- function(inverse) {i <<- inverse}
+        # Returns the value of cached variable i
+        getInverse <- function() {i}
+        # Returns list representing this cached matrix.
+        # A little inconvenient and less than elegant compared to other prog languages
+        list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Inverts matrix x and caches it inside structure created by makeCacheMatrix()
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        # Obtains contents of cached variable corresponding to the inverted matrix
+        i <- x$getInverse()
+        # If variable is not NULL, return its CACHED contents (inverted matrix x)
+        # and control back to caller
+        if(!is.null(i)) {
+                message("getting cached solved matrix")
+                return(i)
+        }
+        # If not, obtain contents of cached variable corresponding to matrix in argument
+        data <- x$get()
+        # Inverts matrix
+        i <- solve(data)
+        # Sets inverted matrix in cached variable
+        x$setInverse(i)
+        # return result, which can be assigned to another variable.
+        i
 }
